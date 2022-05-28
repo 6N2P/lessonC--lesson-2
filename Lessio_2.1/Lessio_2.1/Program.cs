8a7871fafa1eac0459daf65ba4c6367c4027fe17
6N2P <umnik1985@mail.ru>
2022-05-29 00:23:56 +0500

    initial

---
+++ Lessio_2.1/Lessio_2.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lessio_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName = "Иванов Сергей Владимирович";            
+            int age = 32;
+            string email = "sergey@mail.ru";
+            double balyProgram = 5.8;
+            double balyMath = 4.6;
+            double balyPhysics = 2.5;
+            double sum;
+            double middleBal;
+
+            string patern = "Имя: {0};\nВозраст: {1};\nПочта: {2};\nБаллы по программированию: {3};" +
+                "\nБаллы по математематике: {4};\nБаллы по физике: {5};";
+
+            Console.WriteLine(patern, fullName, age, email, balyProgram, balyMath, balyPhysics);
+            Console.ReadKey();
+
+            sum = balyProgram + balyMath + balyPhysics;
+            string dFormted = sum.ToString("#.#");
+            middleBal = sum / 3;
+
+            Console.WriteLine("Сумма балов: " + dFormted);
+            Console.ReadKey();
+            Console.WriteLine("Среднее арифметическое значение: {0}",middleBal);
+
+        }
+    }
+}
---
+++ Lesson_2.2/Lesson_2.2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Lesson_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создаю переменный
+            string name;
+            int age;
+            int growth;
+            double gradeHistory;
+            double gradeMath;
+            double gradeRussianLanguage;
+            double midlleGrade;
+
+            //Запрашиваю данные от пользователя
+            Console.WriteLine("Введите своё имя");
+            name=Console.ReadLine();
+            Console.WriteLine("Введите свой возраст");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите свой рост");
+            growth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите свой бал по истории");
+            gradeHistory = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите свой бал по математике");
+            gradeMath = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите свой бал по русскому языку");
+            gradeRussianLanguage = Convert.ToDouble(Console.ReadLine());
+
+           /* Вычисляю среднюю оценку
+             */
+            midlleGrade = (gradeHistory + gradeMath + gradeRussianLanguage) / 3;
+
+            
+
+            //вывожу на экран результаты
+            Console.WriteLine("Имя: "+name+"\nВозраст: "+age);
+            Console.WriteLine("Бал по истории: {0} \nБал по математике {1} \nБал по рускому языку: {2} ",gradeHistory,gradeMath,gradeRussianLanguage);
+            Console.WriteLine($"Средний бал: {midlleGrade}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
